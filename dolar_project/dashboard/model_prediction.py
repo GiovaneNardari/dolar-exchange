02675acbca2fe8d6de81ecfc259f9ddfa0e675b4
GiovaneBrunoNardari <giovanenardari@gmail.com>
2023-12-01 11:01:59 -0300

    alterando holt por arvore

--- dolar_project/dashboard/model_prediction.py
+++ dolar_project/dashboard/model_prediction.py
@@ -1,4 +1,5 @@
 from statsmodels.iolib.smpickle import load_pickle
+from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 import psycopg2
 import matplotlib.pyplot as plt
@@ -11,17 +12,31 @@ PASSWORD = os.getenv('POSTGRES_PASSWORD', 'None')
 DB = os.getenv('POSTGRES_DB', 'None')
 
 def predict_from_model():
-    with open('serialized_model.pkl', 'rb') as model_file:
-        model = pickle.load(model_file)
+    #with open('serialized_model.pkl', 'rb') as model_file:
+    #    model = pickle.load(model_file)
 
     conn_string = f"host='{HOST}' port='{PORT}' dbname='{DB}' user='{USER}' password='{PASSWORD}'"
     conn = psycopg2.connect(conn_string)
     db_cursor = conn.cursor()
+
+    #treino e predict da árvore
+    db_cursor.execute("SELECT date_registered, price FROM dashboard_dolarprice ORDER BY date_registered DESC")
+    train_data = db_cursor.fetchall()
+    df_train = pd.DataFrame(train_data, columns=['date_registered', 'price'])
+    df_temp= df[['price']].copy()
+    p = 5
+    for i in range(p):
+        dftemp[f'price{i+1}'] = df_temp.price.shift(i+1)
+    X = df_temp.drop('price', axis=1)
+    y = df_temp.price
+    model = DecisionTreeRegressor()
+    model.fit(X, y)
+
     db_cursor.execute("SELECT date_registered, price FROM dashboard_dolarprice ORDER BY date_registered DESC LIMIT 60")
     data = db_cursor.fetchall()
-    
     df_true = pd.DataFrame(data, columns=['date_registered', 'price'])
-    forecast = model.forecast(14)
+    
+    forecast = model.predict(df_true.price)
     
     plt.plot(df_true)
     plt.plot(forecast)
